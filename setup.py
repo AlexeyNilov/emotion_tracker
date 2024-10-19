55a41f8e7e8e3c26b813b8d54db9aa6d61c8237c
alexey.nilov <alexey.nilov@ringcentral.com>
2024-10-19 20:05:47 +0200

    make package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = "0.0.10"
+DESCRIPTION = "Feelings DB"
+
+# Setting up
+setup(
+    name="feelings",
+    version=VERSION,
+    description=DESCRIPTION,
+    include_package_data=True,
+    packages=find_packages(exclude=["conf", "sample", "test"]),
+    install_requires=["pyyaml"],
+    python_requires=">=3.9",
+)
